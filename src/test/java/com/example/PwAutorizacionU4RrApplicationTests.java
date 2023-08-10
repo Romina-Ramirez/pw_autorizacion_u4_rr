dd91816e9f6bd7c11aacddbdc4737189c71268ff
Romina-Ramirez <romina.maite.rt@gmail.com>
2023-08-09 19:48:45 -0500

    Taller 38

---
+++ src/test/java/com/example/PwAutorizacionU4RrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionU4RrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
